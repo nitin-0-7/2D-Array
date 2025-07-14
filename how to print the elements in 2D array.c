e8856728ffc85ff3bf8ad8d3f7c812d4216a316b
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-14 16:38:42 +0530

    how to print 2D Array

---
+++ how to print the elements in 2D array.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][3]={{1,2,3},{3,4,5},{4,5,6}};
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+
+    }
+    
+    return 0;
+}
