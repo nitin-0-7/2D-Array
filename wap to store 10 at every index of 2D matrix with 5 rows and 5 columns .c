5034a3a668da53fca55e00b1a77cac4c8358bb7a
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-15 15:25:01 +0530

    wap to store 10 at every index of 2D matrix with 5 rows and 5 columns

---
+++ wap to store 10 at every index of 2D matrix with 5 rows and 5 columns .c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            arr[i][j]=10;
+        }
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}   
